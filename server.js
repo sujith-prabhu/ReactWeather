78a799bc574fd89b9155d4b3e32f48fae42a2de9
Sujith Prabhu <sujith.prabhu@gmail.com>
2016-08-31 20:10:11 +0100

    https forward code added

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 //applies to every request
 //SpMistake: app.use(req, res, next){ //missing function
-// app.use(function(req, res, next){
-//   /* OpenWeatherMap free version only supports, http but heroku works only https.
-//   So, use the middleware to convert all the https to http.
-//   */
-//   debugger;
-//   if(req.headers["x-forwarded-proto"] ==='http' ){
-//     next();
-//   }else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+app.use(function(req, res, next){
+  /* OpenWeatherMap free version only supports, http but heroku works only https.
+  So, use the middleware to convert all the https to http.
+  */
+  debugger;
+  if(req.headers["x-forwarded-proto"] ==='http' ){
+    next();
+  }else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
