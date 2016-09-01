37f4bf6662069b15999e5372864026a7d8b92727
Sujith Prabhu <sujith.prabhu@gmail.com>
2016-09-01 12:35:15 +0100

    styled the about page. L-54

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,6 +12,18 @@ var React = require('react');
 //   return(<h2>This is About</h2>);
 // }
 
-var About = (props) => <h2>This is About - as statelss function</h2>;
+var About = (props) => <div>
+    <h1 className="text-center">About page</h1>
+    <p> Welcome to the about page. I have developed this for the Complete React course</p>
+    <p>Here are some of the tools used:</p>
+    <ul>
+      <li>
+        <a href="facebook.com/react">React JS</a><p>This is the JS framework used</p>
+      </li>
+      <li>
+        <a href="openweathermap.com">Open weather map</a><p>This is the weather API</p>
+      </li>
+    </ul>
+</div>
 
 module.exports = About;
